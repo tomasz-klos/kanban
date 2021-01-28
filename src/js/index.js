c508952ed972b40c1c608dc66a2d71e07329bde7
tomasz-klos <xilent98@wp.pl>
2021-01-28 16:40:18 +0100

    JS: add function to deleteButton & completeButton

--- src/js/index.js
+++ src/js/index.js
@@ -7,13 +7,16 @@ import '../scss/main.scss';
 /* place your code below */
 
 //////////// Selectors
-const taskInput = document.querySelector(".todoInput");
-const taskButton = document.querySelector(".todoButton");
-const taskList = document.querySelector(".todoList");
+const taskInput = document.querySelector(".taskInput");
+const taskButton = document.querySelector(".taskButton");
+const taskList = document.querySelector(".taskList");
 
 ///////////// Event Listeners
 
 taskButton.addEventListener("click", addTask);
+taskList.addEventListener("click", deleteTask);
+taskList.addEventListener("click", completeTask);
+
 
 ///////////// Functions
 
@@ -22,25 +25,42 @@ function addTask(event) {
     const taskDiv = document.createElement("div");
     taskDiv.classList.add("taskSection__div");
     const newTask = document.createElement('li');
-    newTask.innerText = 'Task';
+    newTask.innerText = taskInput.value;
     newTask.classList.add('taskItem');
     taskDiv.appendChild(newTask);
 
     ////////// Completed button
-
     const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<img src="https://raw.githubusercontent.com/tomasz-klos/kanban/master/src/assets/img/Checkmark.png">';
+    completedButton.innerHTML = '<img class="checkmark" src="https://raw.githubusercontent.com/tomasz-klos/kanban/b422b9c55237de332b0bd145aa63e29bac444d9e/src/assets/img/Checkmark.svg">';
     completedButton.classList.add("completeButton");
     taskDiv.appendChild(completedButton);
 
     ////////// Delete button
-
     const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<img src="https://raw.githubusercontent.com/tomasz-klos/kanban/d1dc6f3b939beba0441e0fd42ebde29dfb933890/src/assets/img/trash.svg">';
-    deleteButton.classList.add("completeButton");
+    deleteButton.innerHTML = '<img class="trash" src="https://raw.githubusercontent.com/tomasz-klos/kanban/d1dc6f3b939beba0441e0fd42ebde29dfb933890/src/assets/img/trash.svg">';
+    deleteButton.classList.add("deleteButton");
     taskDiv.appendChild(deleteButton);
 
+    ////////// Add div to list
     taskList.appendChild(taskDiv);
 
-}
+    ///////// Clear input
+    taskInput.value = '';
 
+};
+
+function deleteTask(e){
+    const item = e.target;
+    if (item.classList[0] === "deleteButton") {
+        const task = item.parentElement;
+        task.remove();
+    };
+};
+
+function completeTask(e){
+    const item = e.target;
+    if (item.classList[0] === "completeButton") {
+        const task = item.parentElement;
+        task.classList.toggle("completed");
+    };
+};
