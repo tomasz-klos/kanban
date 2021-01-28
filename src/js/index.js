b9a242995579f0b69e8683c5f4e47652dae75ee5
tomasz-klos <xilent98@wp.pl>
2021-01-28 14:35:02 +0100

    change taskSection css

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,9 @@ import '../scss/main.scss';
 /* place your code below */
 
 //////////// Selectors
-const taskInput = document.querySelector(".todo-input");
-const taskButton = document.querySelector(".todo-button");
-const taskList = document.querySelector(".todo-list");
+const taskInput = document.querySelector(".todoInput");
+const taskButton = document.querySelector(".todoButton");
+const taskList = document.querySelector(".todoList");
 
 ///////////// Event Listeners
 
@@ -20,24 +20,24 @@ taskButton.addEventListener("click", addTask);
 function addTask(event) {
     event.preventDefault();
     const taskDiv = document.createElement("div");
-    taskDiv.classList.add("todo-section__div");
+    taskDiv.classList.add("taskSection__div");
     const newTask = document.createElement('li');
     newTask.innerText = 'Task';
-    newTask.classList.add('todo-item');
+    newTask.classList.add('taskItem');
     taskDiv.appendChild(newTask);
 
     ////////// Completed button
 
     const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<img src="../assets/img/button-unchecked.svg">';
-    completedButton.classList.add("complete-btn");
+    completedButton.innerHTML = '<img src="https://raw.githubusercontent.com/tomasz-klos/kanban/d1dc6f3b939beba0441e0fd42ebde29dfb933890/src/assets/img/button-unchecked.svg">';
+    completedButton.classList.add("completeButton");
     taskDiv.appendChild(completedButton);
 
     ////////// Delete button
 
     const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<img src="../assets/img/trash.svg">';
-    deleteButton.classList.add("complete-btn");
+    deleteButton.innerHTML = '<img src="https://raw.githubusercontent.com/tomasz-klos/kanban/d1dc6f3b939beba0441e0fd42ebde29dfb933890/src/assets/img/trash.svg">';
+    deleteButton.classList.add("completeButton");
     taskDiv.appendChild(deleteButton);
 
     taskList.appendChild(taskDiv);
