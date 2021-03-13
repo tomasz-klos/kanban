8d7b46ea61a547f7aa23153dc2e622b3188d7b52
tomasz-klos <xilent98@wp.pl>
2021-03-13 03:39:15 +0100

    JS new functions, deleteTask,add sweetalert and more

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,10 @@ import "../scss/main.scss";
 
 import Swiper from "swiper/bundle";
 
+import Swal from "sweetalert2/dist/sweetalert2.js";
+
+import "sweetalert2/src/sweetalert2.scss";
+
 // init Swiper:
 
 // uncomment the lines below to enable PWA
@@ -12,7 +16,7 @@ import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
-
+Swal.fire("Hello world!");
 ////////////////////////////////////////////////////////// Selectors ////////////////////////////////////////////////////////////////
 
 const taskInput = document.querySelector(".taskInput");
@@ -52,13 +56,19 @@ const work = "work";
 const personal = "personal";
 const learning = "learning";
 const priority = "priority";
+const normal = "normal";
+const workPriority = "workPriority";
+const personalPriority = "personalPriority";
+const learningPriority = "learningPriority";
 
 const numberTasksWork = document.querySelector(".numberOfTaskWork--js");
 const numberTasksPersonal = document.querySelector(".numberOfTaskPersonal--js");
 const numberTasksLearning = document.querySelector(".numberOfTaskLearning--js");
 
-const typeTaskInput = document.querySelector(".typeTaskSelect--js");
-const categorySelect = document.querySelector(".categoryTask--js");
+const typeTaskInput = document.getElementsByName("typeTask");
+let typeTaskInputValue;
+const categoryInput = document.getElementsByName("categoryTask");
+let categoryInputValue;
 
 const hamburgerButton = document.querySelector(".hamburger--js");
 const menuProfile = document.querySelector(".menuProfile--js");
@@ -147,14 +157,12 @@ document.addEventListener(
 );
 
 taskButton.addEventListener("click", addTask);
-// taskList.addEventListener("click", deleteTask.bind(event, priority, work));
-// taskList.addEventListener("click", deleteTask.bind(event, priority, personal));
-// taskList.addEventListener("click", deleteTask.bind(event, priority, learning));
+taskList.addEventListener("click", deleteTask);
 taskList.addEventListener("click", completeTask);
 
-workList.addEventListener("click", deleteTask.bind(event, work));
-personalList.addEventListener("click", deleteTask.bind(event, personal));
-learningList.addEventListener("click", deleteTask.bind(event, learning));
+workList.addEventListener("click", deleteTask);
+personalList.addEventListener("click", deleteTask);
+learningList.addEventListener("click", deleteTask);
 
 workList.addEventListener("click", completeTask);
 personalList.addEventListener("click", completeTask);
@@ -216,27 +224,37 @@ function addTask(event) {
   const workList = document.querySelector(".workTask--js");
   const personalList = document.querySelector(".personalTask--js");
   const learningList = document.querySelector(".learningTask--js");
-  switch (typeTaskInput.value) {
+  for (let i = 0; i < typeTaskInput.length; i++) {
+    if (typeTaskInput[i].checked) {
+      typeTaskInputValue = typeTaskInput[i].value;
+    }
+  }
+  for (let i = 0; i < categoryInput.length; i++) {
+    if (categoryInput[i].checked) {
+      categoryInputValue = categoryInput[i].value;
+    }
+  }
+  switch (typeTaskInputValue) {
     case "priority":
-      switch (categorySelect.value) {
+      switch (categoryInputValue) {
         case "work":
-          createTask(taskList, taskInput.value);
-          createTask(workList, taskInput.value);
+          createTask(taskList, taskInput.value, workPriority);
+          createTask(workList, taskInput.value, priority);
           saveTasksInLocalStorage(taskInput.value, work);
           saveTasksInLocalStorage(taskInput.value, priority);
           closeSection(addTaskSection);
 
           break;
         case "personal":
-          createTask(taskList, taskInput.value);
-          createTask(personalList, taskInput.value);
+          createTask(taskList, taskInput.value, personalPriority);
+          createTask(personalList, taskInput.value, priority);
           saveTasksInLocalStorage(taskInput.value, personal);
           saveTasksInLocalStorage(taskInput.value, priority);
           closeSection(addTaskSection);
           break;
         case "learning":
-          createTask(taskList, taskInput.value);
-          createTask(learningList, taskInput.value);
+          createTask(taskList, taskInput.value, learningPriority);
+          createTask(learningList, taskInput.value, priority);
           saveTasksInLocalStorage(taskInput.value, learning);
           saveTasksInLocalStorage(taskInput.value, priority);
           closeSection(addTaskSection);
@@ -244,21 +262,21 @@ function addTask(event) {
       }
       break;
     case "normal":
-      switch (categorySelect.value) {
+      switch (categoryInputValue) {
         case "work":
-          createTask(workList, taskInput.value);
+          createTask(workList, taskInput.value, normal);
           saveTasksInLocalStorage(taskInput.value, work);
           closeSection(addTaskSection);
           openSection(workCategorySection);
           break;
         case "personal":
-          createTask(personalList, taskInput.value);
+          createTask(personalList, taskInput.value, normal);
           saveTasksInLocalStorage(taskInput.value, personal);
           closeSection(addTaskSection);
           openSection(personalCategorySection);
           break;
         case "learning":
-          createTask(learningList, taskInput.value);
+          createTask(learningList, taskInput.value, normal);
           saveTasksInLocalStorage(taskInput.value, learning);
           closeSection(addTaskSection);
           openSection(learningCategorySection);
@@ -270,30 +288,178 @@ function addTask(event) {
   numberOfTask(workList, numberTasksWork);
   numberOfTask(personalList, numberTasksPersonal);
   numberOfTask(learningList, numberTasksLearning);
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your task has been saved",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 }
 
-function deleteTask(name, category, e) {
+function deleteTask(e) {
   const item = e.target;
+  const task = item.parentElement;
+  const categoryList = task.parentElement;
   if (item.classList[0] === "deleteButton") {
-    const task = item.parentElement;
-    removeTasksFromLocalStorage(task, name);
-    task.remove();
-    if (name === "priority") {
-      removeTasksFromLocalStorage(task, category);
-    } else {
-      console.log("AUUUUUUUUUUU");
-    }
-    numberOfTask(workList, numberTasksWork);
-    numberOfTask(personalList, numberTasksPersonal);
-    numberOfTask(learningList, numberTasksLearning);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const childrenTaskList = taskList.childNodes;
+
+        for (let i = 0; i < childrenTaskList.length; i++) {
+          let nameTask = childrenTaskList[i].firstChild.textContent;
+          let taskChild = task.firstChild.textContent;
+          console.log(taskChild);
+          if (nameTask == taskChild){
+            console.log(childrenTaskList[i]);
+            console.log(childrenTaskList[i].classList[1]);
+            if (childrenTaskList[i].classList[1] === "workPriority"){
+              removePriorityFromLocalStorage(childrenTaskList[i], priority);
+              removeTasksFromLocalStorage(childrenTaskList[i], work);
+            } else if (childrenTaskList[i].classList[1] === "personalPriority"){
+              removePriorityFromLocalStorage(childrenTaskList[i], priority);
+              removeTasksFromLocalStorage(childrenTaskList[i], personal);
+            } else if (childrenTaskList[i].classList[1] === "learningPriority"){
+              removePriorityFromLocalStorage(childrenTaskList[i], priority);
+              removeTasksFromLocalStorage(childrenTaskList[i], learning);
+            } else {
+              console.log("Something wrong");
+            }
+            childrenTaskList[i].remove();
+          } else {
+            console.log("not work");
+          }
+          // if (nameTask.textContent) {
+          //   typeTaskInputValue = typeTaskInput[i].value;
+          // }
+        }
+        console.log(task);
+
+        task.remove();
+
+        // if (worklistArr.includes(task) === true) {
+        //   console.log(workList);
+        //   console.log("Great success");
+        // } else {
+        //   console.log("Very bad");
+        // }
+
+
+        if (task.classList[1] === "priority") {
+          if (categoryList.classList[1] === "workList") {
+            removeTasksFromLocalStorage(task, work);
+            removeTasksFromLocalStorage(task, priority);
+          } else if (categoryList.classList[1] === "personalList") {
+            removeTasksFromLocalStorage(task, personal);
+            removeTasksFromLocalStorage(task, priority);
+          } else if (categoryList.classList[1] === "learningList") {
+            removeTasksFromLocalStorage(task, learning);
+            removeTasksFromLocalStorage(task, priority);
+          } else {
+          }
+        } else {
+          if (categoryList.classList[1] === "workList") {
+            removeTasksFromLocalStorage(task, work);
+          } else if (categoryList.classList[1] === "personalList") {
+            removeTasksFromLocalStorage(task, personal);
+          } else if (categoryList.classList[1] === "learningList") {
+            removeTasksFromLocalStorage(task, learning);
+          } else {
+          }
+        }
+        Swal.fire("Deleted!", "Your task has been deleted.", "success");
+      }
+      numberOfTask(workList, numberTasksWork);
+      numberOfTask(personalList, numberTasksPersonal);
+      numberOfTask(learningList, numberTasksLearning);
+    });
   }
 }
+
+// function deleteTask(e) {
+//   Swal.fire({
+//     title: "Are you sure?",
+//     text: "You won't be able to revert this!",
+//     icon: "warning",
+//     showCancelButton: true,
+//     confirmButtonColor: "#3085d6",
+//     cancelButtonColor: "#d33",
+//     confirmButtonText: "Yes, delete it!",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       const item = e.target;
+//       if (item.classList[0] === "deleteButton") {
+//         const task = item.parentElement;
+//         const categoryList = task.parentElement;
+//         task.remove();
+//         const worklistArr = [];
+//         worklistArr.push(workList.children);
+//         console.log(worklistArr);
+//         if (worklistArr.includes(task) === true) {
+//           console.log(workList);
+//           console.log("Great success");
+//         } else {
+//           console.log("Very bad");
+//         }
+//       }
+//       Swal.fire("Deleted!", "Your task has been deleted.", "success");
+//     }
+//   });
+//   // if (task.classList[1] === "priority") {
+//   //   if (categoryList.classList[1] === "workList") {
+//   //     removeTasksFromLocalStorage(task, work);
+//   //     removeTasksFromLocalStorage(task, priority);
+//   //   } else if (categoryList.classList[1] === "personalList") {
+//   //     removeTasksFromLocalStorage(task, personal);
+//   //     removeTasksFromLocalStorage(task, priority);
+//   //   } else {
+//   //     removeTasksFromLocalStorage(task, learning);
+//   //     removeTasksFromLocalStorage(task, priority);
+//   //   }
+//   // } else {
+//   //   if (categoryList.classList[1] === "workList") {
+//   //     removeTasksFromLocalStorage(task, work);
+//   //   } else if (categoryList.classList[1] === "personalList") {
+//   //     removeTasksFromLocalStorage(task, personal);
+//   //   } else {
+//   //     removeTasksFromLocalStorage(task, learning);
+//   //   }
+//   // }
+
+//   numberOfTask(workList, numberTasksWork);
+//   numberOfTask(personalList, numberTasksPersonal);
+//   numberOfTask(learningList, numberTasksLearning);
+// }
 
 function completeTask(e) {
   const item = e.target;
   if (item.classList[0] === "completeButton") {
     const task = item.parentElement;
     task.classList.toggle("completed");
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: "success",
+      title: "Signed in successfully",
+    });
   }
 }
 
@@ -346,21 +512,23 @@ function closeSection(section) {
   mainSection.style.display = "flex";
 }
 
-function createTask(list, value) {
+function createTask(list, value, name) {
   const taskDiv = document.createElement("div");
   const completedButton = document.createElement("button");
   const newTask = document.createElement("li");
   const deleteButton = document.createElement("button");
   taskDiv.classList.add("priorityTask__div");
+  taskDiv.classList.add(name);
+
+  newTask.innerText = value;
+  newTask.classList.add("taskItem");
+  taskDiv.appendChild(newTask);
+
   ////////// Completed button
   completedButton.innerHTML =
     '<img class="checkmark" src="https://raw.githubusercontent.com/tomasz-klos/kanban/b422b9c55237de332b0bd145aa63e29bac444d9e/src/assets/img/Checkmark.svg">';
   completedButton.classList.add("completeButton");
   taskDiv.appendChild(completedButton);
-
-  newTask.innerText = value;
-  newTask.classList.add("taskItem");
-  taskDiv.appendChild(newTask);
 
   //////// Delete button
   deleteButton.innerHTML =
@@ -370,14 +538,6 @@ function createTask(list, value) {
 
   ////////// Add div to list
   list.appendChild(taskDiv);
-
-  // if (typeTaskInput.value === "priority" && categorySelect.value === "work"){
-  //   taskDiv.classList.add("workPriority");
-  // } else if (typeTaskInput.value === "priority" && categorySelect.value === "personal"){
-  //   taskDiv.classList.add("personalPriority");
-  // } else if (typeTaskInput.value === "priority" && categorySelect.value === "learning"){
-  //   taskDiv.classList.add("learningPriority");
-  // };
 }
 
 function welcome() {
@@ -484,8 +644,13 @@ function getWorkTask() {
   } else {
     tasks = JSON.parse(localStorage.getItem("work"));
   }
+  const arr = localStorage.getItem("priority");
   tasks.forEach(function (task) {
-    createTask(workList, task);
+    if (arr.includes(task) === true) {
+      createTask(workList, task, priority);
+    } else {
+      createTask(workList, task, normal);
+    }
   });
 }
 
@@ -496,8 +661,13 @@ function getPersonalTask() {
   } else {
     tasks = JSON.parse(localStorage.getItem("personal"));
   }
+  const arr = localStorage.getItem("priority");
   tasks.forEach(function (task) {
-    createTask(personalList, task);
+    if (arr.includes(task) === true) {
+      createTask(personalList, task, priority);
+    } else {
+      createTask(personalList, task, normal);
+    }
   });
 }
 
@@ -508,8 +678,13 @@ function getLearningTask() {
   } else {
     tasks = JSON.parse(localStorage.getItem("learning"));
   }
+  const arr = localStorage.getItem("priority");
   tasks.forEach(function (task) {
-    createTask(learningList, task);
+    if (arr.includes(task) === true) {
+      createTask(learningList, task, priority);
+    } else {
+      createTask(learningList, task, normal);
+    }
   });
 }
 
@@ -521,7 +696,17 @@ function getPriorityTask() {
     tasks = JSON.parse(localStorage.getItem("priority"));
   }
   tasks.forEach(function (task) {
-    createTask(taskList, task);
+    const lsPriority = localStorage.getItem("priority");
+    const lsWork = localStorage.getItem("work");
+    const lsPersonal = localStorage.getItem("personal");
+    const lsLearning = localStorage.getItem("learning");
+    if (lsPriority.includes(task) === true && lsWork.includes(task) === true){
+      createTask(taskList, task, workPriority);
+    } else if (lsPriority.includes(task) === true && lsPersonal.includes(task) === true){
+      createTask(taskList, task, personalPriority);
+    } else if (lsPriority.includes(task) === true && lsLearning.includes(task) === true){
+      createTask(taskList, task, learningPriority);
+    };
   });
 }
 
@@ -532,8 +717,26 @@ function removeTasksFromLocalStorage(task, name) {
   } else {
     tasks = JSON.parse(localStorage.getItem(name));
   }
-  const taskIndex = task.children[0].innerText;
+  const taskIndex = task.children[1].innerText;
+  console.log(taskIndex);
   tasks.splice(tasks.indexOf(taskIndex), 1);
+  localStorage.setItem(name, JSON.stringify(tasks));
+}
+
+function removePriorityFromLocalStorage(task, name) {
+  let tasks;
+  if (localStorage.getItem(name) === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem(name));
+  }
+  console.log(task);
+  console.log(tasks);
+  const nameTask = task.firstChild.textContent;
+  const taskIndex = task.children[1].innerText;
+  console.log(taskIndex);
+  tasks.splice(tasks.indexOf(nameTask), 1);
+  console.log(tasks);
   localStorage.setItem(name, JSON.stringify(tasks));
 }
 
